14251d68bb5950bb5eeca6cfcd4747ecbed9b7bf
Asim Poudel <ap03232822@student.ku.edu.np>
2024-06-04 15:46:33 +0545

    Add files via upload

--- mt.cpp
+++ mt.cpp
@@ -1,18 +1,74 @@
 
 #include <random>
 #include <iostream>
+#include <ctime>
+#include <chrono>
+
 using namespace std;
 
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+void calculateTimeComplexity(int arr[], int n) {
+    auto start = chrono::high_resolution_clock::now(); // Start measuring time
+
+    insertionSort(arr,n );
+
+    auto end = chrono::high_resolution_clock::now(); // Stop measuring time
+    auto duration = chrono::duration_cast<chrono::microseconds>(end - start); // Calculate the duration in microseconds
+
+    cout << "Time Complexity of InsertionSort: " << duration.count() << "microseconds" << endl;
+}
+
 int main(){
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n],revArr[n];
      random_device rd;
      mt19937 gen(rd());
      uniform_int_distribution<> dis(0, 999);
-     cout << "Using MT:\n" <<  endl;
-    for(int i = 0; i < 100; i++) {
-         cout << dis(gen) <<  endl;
+    for(int i = 0; i < n; i++) {
+         arr[i]= dis(gen);
     }
-     cout <<  endl;
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+    
+    for(int i = 0; i < n; i++) {
+         revArr[n-i-1]=arr[i];
+    }
+
+    calculateTimeComplexity(arr, n);
+    calculateTimeComplexity(revArr, n);
+   
+
     return 0;
+
+
+
 }
 
 
